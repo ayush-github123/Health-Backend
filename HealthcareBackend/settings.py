0f734ee6f0e35bda9c396be135f39c799e0af249
Ayush Rai <raiayush3366@gmail.com>
2025-03-26 12:55:23 +0530

    Allowed Render domain to Host

--- HealthcareBackend/settings.py
+++ HealthcareBackend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["health-backend-gjoo.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
